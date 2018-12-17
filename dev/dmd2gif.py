b89f110a815111d41090df389378bfc47319e054
Michael Betz <michibetz@gmail.com>
2018-12-17 00:19:49 -0800

    Fix dmd2gif.py

--- dev/dmd2gif.py
+++ dev/dmd2gif.py
@@ -1,110 +1,95 @@
 #!/usr/bin/python3
 """
-	convert extracted runDMD image file to a series of .gif files
-	usage:
-		dmd2gif.py ./dmd.img ./outFolder
+    convert extracted runDMD image file to a series of .gif files
+    usage:
+        dmd2gif.py ./dmd.img ./outFolder
 """
 import sys
-from numpy import array, zeros, uint8
+from os.path import join
+from struct import unpack
 from PIL import Image
 
+HEADER_OFFS = 0x0000C800
+HEADER_SIZE = 0x00000200
+ANI_DAT_START = 0x004E2000
+
 if len(sys.argv) != 3:
-	print(__doc__)
-	sys.exit()
+    print(__doc__)
+    sys.exit()
 
 fName = sys.argv[1]
 outFolder = sys.argv[2]
 
-def getFrame( buf, xSize=128, ySize=32, frameOffset=0, byteOffset=None ):
-    """ returns unpacked image data, 1 byte per pixel """
-    if byteOffset is None:
-        byteOffset = xSize*ySize*frameOffset
-    rawDat = buf[byteOffset:byteOffset+ySize*xSize//2]
+
+def parse_header(f, headerIndex):
+    """
+      f: file object of RunDMD_BXXX.img
+      headerIndex: integer, which animation to fetch
+      returns: dict with header info
+    """
+    f.seek(HEADER_OFFS + HEADER_SIZE * headerIndex)
+    rawDat = unpack(">HxBIBBB9x32s", f.read(52))
+    header = {
+        "animation_id": rawDat[0],
+        "n_bitmaps": rawDat[1],
+        "byte_offset": rawDat[2] * HEADER_SIZE,
+        "n_frames": rawDat[3],
+        "width": rawDat[4],
+        "height": rawDat[5],
+        "name": rawDat[6].strip(b'\x00').decode("ascii")
+    }
+    f.seek(header["byte_offset"])
+    buff = f.read(header["n_frames"] * 2)
+    header["frame_ids"] = list(buff[0::2])
+    header["frame_durs"] = list(buff[1::2])
+    return header
+
+
+def get_frame_dat(f, header, frame_offset):
+    """
+        f: open .img file
+        header: dict from parse_header()
+        frame_offset: which frame (0 = first)
+        returns byte buffer of size 128 x 64, 1 pixel per byte
+    """
+    byte_offset = header["byte_offset"] + HEADER_SIZE
+    byte_offset += 128 * 32 * frame_offset // 2
+    f.seek(byte_offset)
+    buff_4 = f.read(128 * 32 // 2)      # 2 pixel per byte
     # unpack 2 pixels / byte to 1 pixel / byte
-    a = zeros(xSize*ySize, dtype=uint8)
-    a[1::2] = rawDat & 0x0F
-    a[0::2] = rawDat >> 4
-    return a.reshape((ySize,xSize))
+    buff_8 = bytearray(128 * 32)        # 1 pixel per byte
+    buff_8[0::2] = [(i & 0xF0) for i in buff_4]
+    buff_8[1::2] = [(i & 0x0F) << 4 for i in buff_4]
+    return bytes(buff_8)
 
-def isFooter( dat ):
-    """ returns >= 0 if a footer frame of size 0x200 bytes is detected """
-    # we search for this sequence: xx 01 xx 02 xx 03 xx 04 xx
-    tag = b"\x01\x02\x03\x04"
-    a = b"".join( dat.flatten()[:frameSize][0::2] )
-    b = b"".join( dat.flatten()[:frameSize][1::2] )
-    ind = a.find( tag )
-    if ind >= 0:
-        return 2*ind
-    ind = b.find( tag )
-    if ind >= 0:
-        return 2*ind+1
-    return ind
 
-def getNextFname():
-    """ get next file name from the header table """
-    global fNameOffset
-    fName = b"".join( rawDat[fNameOffset:fNameOffset+32] ).replace(b"\x00",b"").decode("ascii")
-    fNameOffset += 0x200
-    return fName
-
-with open(fName,"rb") as f:
-    rawDat = array( bytearray(f.read()), dtype=uint8 )
-
-xSize = 128
-ySize =  32
-frameSize   = xSize * ySize // 2   # each data byte holds two 4 bit pixels
-offset      = 0x12C400             # Start of first animation
-fNameOffset = 0x00C814             # Start of first filename
-loopOn = {
-    "24_002": 5,
-    "THE_CHAMPION_PUB_002": 5,
-    "THE_CHAMPION_PUB_003": 5,
-    "THE_CHAMPION_PUB_004": 5,
-    "THE_CHAMPION_PUB_007": 3,
-    "THE_CHAMPION_PUB_015": 3,
-    "THE_CHAMPION_PUB_018": 5,
-    "THE_CHAMPION_PUB_025": 5,
-    "THE_CHAMPION_PUB_032": 2,
-    "THE_CHAMPION_PUB_042": 3,
-    "THE_CHAMPION_PUB_044": 5,
-    "THE_CHAMPION_PUB_045": 5,
-    "THE_CHAMPION_PUB_046": 5,
-    "THE_CHAMPION_PUB_049": 5,
-    "THE_CHAMPION_PUB_050": 7,
-    "THE_CHAMPION_PUB_067": 5,
-    "THE_CHAMPION_PUB_068": 5,
-}
-imlist = []
-aniStartOffset = offset
-aniLengthBytes = 0
-with open("offsets.txt","w") as f:
-    while(1):
-        aniDat = rawDat[offset:offset+frameSize]
-        if len(aniDat) < frameSize:
-            print("done")
-            break
-        fOffs = isFooter( aniDat )
-        if fOffs >= 0:
-            # end of current file, save it
-            fName = getNextFname()
-            fullFName = "./gif/{}.gif".format( fName )
-            print( hex(offset), fullFName )
-            f.write("0x{:08x},0x{:08x}\n".format(aniStartOffset, aniLengthBytes) )
-            if fName in loopOn:
-                for im in imlist:
-                    im.info["loop"] = loopOn[fName]
-            imlist[0].save( fullFName, "GIF", optimize=False, save_all=True, append_images=imlist[1:] )
-            #start a new file
-            imlist = []
-            offset += fOffs + 0x200
-            aniStartOffset = offset
-            aniLengthBytes = 0
-        else:
-            imgData = getFrame(aniDat, xSize, ySize) * 16
-            img = Image.fromarray(imgData, mode="L")
-            img.info["background"]   = 0x00*16
-            img.info["transparency"] = 0x0A*16
-            img.info["duration"]     = 1/10 * 1000
-            imlist.append( img )
-            aniLengthBytes += frameSize
-            offset += frameSize    
+with open(fName, "rb") as f:
+    if f.read(3) != b'DGD':
+        raise RuntimeError('Invalid DGD header')
+    n_ani = unpack(">H", f.read(2))[0]
+    f.seek(0x1ef)
+    buildStr = f.read(8).strip(b'\x00').decode("ascii")
+    print("Found", n_ani, "animations in", buildStr)
+    for i_ani in range(n_ani):
+        try:
+            h = parse_header(f, i_ani)
+            full_file_name = join(outFolder, h["name"] + ".gif")
+            imgs = []
+            for f_id, f_dur in zip(h["frame_ids"], h["frame_durs"]):
+                img_dat = get_frame_dat(f, h, f_id - 1)
+                img = Image.frombytes("P", (128, 32), img_dat)
+                img.info["duration"] = f_dur
+                imgs.append(img)
+            imgs[0].save(
+                full_file_name,
+                "GIF",
+                save_all=True,
+                append_images=imgs[1:],
+                optimize=False
+                # There seems to be a bug in pillow for transparency :(
+                # transparency=0xA0,
+                # disposal=2
+            )
+            print(full_file_name)
+        except Exception as e:
+            print("Ohh snap ", e, full_file_name)
